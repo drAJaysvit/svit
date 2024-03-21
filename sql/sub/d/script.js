243cf40a0b4d7d2b7d7a03c073c0c8a2358ec0e5
drAJaysvit <109869929+drAJaysvit@users.noreply.github.com>
2024-03-21 15:45:19 +0530

    up

--- sql/sub/d/script.js
+++ sql/sub/d/script.js
@@ -1,4 +1,12 @@
 const cpyall = document.querySelector('.cpy-all')
+cpyall.addEventListener('click',()=>{
+    let content = "";
+    for(let ele in pros){
+        content = content + pros[ele].textContent + "\n\n";
+    }
+    navigator.clipboard.writeText(content);
+    alert("Copied all");
+})
 
 
 
@@ -13,11 +21,3 @@ for (const ele in pros) {
 })
 }
 
-cpyall.addEventListener('click',()=>{
-    let content = "";
-    for(let ele in pros){
-        content = content + pros[ele].textContent + "\n\n";
-    }
-    navigator.clipboard.writeText(content);
-    alert("Copied all");
-})
